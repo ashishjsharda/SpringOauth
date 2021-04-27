836ba2f8ef6587700b1df634f5a3758597b0061f
Ashish Sharda <ashishjsharda@gmail.com>
2021-04-27 07:21:13 -0400

    Add all my files

--- AuthorizationServerApplication/src/main/java/com/okta/spring/AuthorizationServerApplication/AuthServerConfig.java
+++ AuthorizationServerApplication/src/main/java/com/okta/spring/AuthorizationServerApplication/AuthServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 /**
  * Configuration -WIP
@@ -17,9 +18,19 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     private final PasswordEncoder passwordEncoder;
     @Value("${user.oauth.clientId}")
     private String clientId;
+    @Value("${user.oauth.clientSecret}")
+    private String clientSecret;
+    @Value("${user.oauth.redirectUris}")
+    private String redirectUris;
 
 
     public AuthServerConfig(PasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        super.configure(security);
+        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
     }
 }
